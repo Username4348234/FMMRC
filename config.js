3ce208e19dd37708cbc8a413ed952fc6d7034659
Username4348234 <bi@rheserva.com.br>
2026-04-17 11:33:37 -0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.CONFIG = {
   WEBHOOK_URL: "https://webhookbi.rheserva.com.br/webhook",
-  TOKEN: "123456767899080testeprogpt"
+  TOKEN: "7BAxIjoOfEVg55APp6G3MuvixWqlyCblH21OZG1EUjwxE1urWDB0X6lYr9uP"
 };
